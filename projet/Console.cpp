2bd8ec298c4fbc87fb9fd214ae7809b72c5b01d1
Arthur <arthur.machaux@viacesi.fr>
2025-12-01 16:31:07 +0100

    console.h + console.cpp (manque de fonction pour que cela soit opérationnel)

---
+++ projet/Console.cpp
@@ -0,0 +1,33 @@
+#include "Console.h"
+#include "GrilleLoadSave.h"
+#include <iostream>   
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+//Constructeur
+Console::Console(Game& gamePtr, int nbIterations, std::string file)
+	: game(&gamePtr), iterations(nbIterations), inputFile(file) {
+}
+
+void Console::run() {
+	game -> initializeFromFile (inputFile);
+	std::string outputFile = inputFile + "_out";
+
+	if(!fs::exists(outputFile)) {
+		fs::create_directory(outputFile);
+	}
+
+	std::cout << "Mode Console: Calcul de" << iterations << "iterations..." << std::endl;
+
+	for (int i = 0; i < iterations; i++) {
+		game->update();
+
+		std::string outFile = outputFile + "iter_" + std::to_string(i + 1) + ".txt";
+
+		GridLoadSave::save(game->getGrid(), outFile);
+
+		std::cout << "Iteration" << (i + 1) << "sauvegardee." << std::endl;
+	}
+	std::cout << "Termine." << std::endl;
+}
--- projet/Console.h
+++ projet/Console.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "Game.h"
+#include <string>
+
+class Console {
+private:
+	Game* game;
+	int iterations;
+	std::string inputFile;
+
+public:
+	Console(Game& gamePtr, int nbIterations, std::string file);
+	void run();
+
+};
+
