83c9ae2e90031361bd11580632c9ca242dee50f6
Arthur BLOT <arthur.blot@viacesi.fr>
2025-12-05 14:43:32 +0100

    essaie sourie dessin glisser

--- projet/UI.cpp
+++ projet/UI.cpp
@@ -8,7 +8,8 @@ UI::UI()
 
 
 void UI::show() {
-	GridRender gr(game.getGrid()); //ajout d'un argument dans ce constructeur qui emetait une erreur empechant le lancement du code
+    GridRender gr(game.getGrid()); //ajout d'un argument dans ce constructeur qui emetait une erreur empechant le lancement du code
+    sf::Vector2i lastEditedPos(-1, -1);
     sf::Texture playButtonNotPressed("playButtonNotPressed.png");
     sf::Sprite playButtonNotPressedSprite(playButtonNotPressed);
     playButtonNotPressedSprite.setPosition(sf::Vector2f(650, 50));
@@ -25,37 +26,33 @@ void UI::show() {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
-
-            // clic souris pour creer ou tuer des cellules 
-            if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>())
-            {
-                if (mouseButtonPressed->button == sf::Mouse::Button::Left)
-                {
-                    std::cout << "the right button was pressed" << std::endl;
-                    std::cout << "mouse x: " << mouseButtonPressed->position.x << std::endl;
-                    std::cout << "mouse y: " << mouseButtonPressed->position.y << std::endl;
-                    sf::Vector2i gridPos = gr.convertWindowCoordinateToGridCoordinate(mouseButtonPressed->position);
-                    std::cout << "grid x: " << gridPos.x << std::endl;
-                    std::cout << "grid y: " << gridPos.y << std::endl;
-                    //checker que gridPos est dans les dimensions de la grille
-                    Cell& c = game.getGrid().GetCell(gridPos.x, gridPos.y); 
-                    c.setIsAlive(!c.getIsAlive());
-                }
-            }
-
-            //ajout de quand fleche de droite appuyee on avance de 1 iteration 
-            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
-            {
-                if (keyPressed->code == sf::Keyboard::Key::Right)
-                {
+            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->code == sf::Keyboard::Key::Right) {
                     game.iteration();
                     std::cout << "Iteration avancee par fleche droite." << std::endl;
+
+                    // clic souris pour creer ou tuer des cellules 
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                        sf::Vector2i gridPos = gr.convertWindowCoordinateToGridCoordinate(mousePos);
+                        std::cout << "grid x: " << gridPos.x << std::endl;
+                        std::cout << "grid y: " << gridPos.y << std::endl;
+                        //checker que gridPos est dans les dimensions de la grille
+                        Grid& grid = game.getGrid(); // Référence raccourcie
+                        if (gridPos.x >= 0 && gridPos.x < grid.GetWidth() && gridPos.y >= 0 && gridPos.y < grid.GetHeight()) {
+                            if (gridPos != lastEditedPos) {
+                                Cell& c = game.getGrid().GetCell(gridPos.x, gridPos.y);
+                                c.setIsAlive(!c.getIsAlive());
+                                lastEditedPos = gridPos;
+                            }
+                        }
+                    }
                 }
             }
+            window.clear(sf::Color::Black);
+            gr.drawGrid(window, game.getGrid());
+            window.draw(playButtonNotPressedSprite);
+            window.display();
         }
-        window.clear(sf::Color::Black);
-        gr.drawGrid(window, game.getGrid());
-        window.draw(playButtonNotPressedSprite);
-        window.display();
     }
 }
